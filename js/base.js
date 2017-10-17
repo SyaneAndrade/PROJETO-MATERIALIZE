91d41ffdada162211ca27f04118f8b7a9d6fbdaa
Joel Andrade <joel.a.rocha@hotmail.com>
2017-10-17 14:00:59 -0200

    menu , e card

--- js/base.js
+++ js/base.js
@@ -20,3 +20,33 @@ $(document).ready(function () {
         height:657
     });
 });
+//scrol
+
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('navbar-bg');
+  } else {
+    $('nav').removeClass('navbar-bg');
+  }
+});
+
+
+jQuery(document).ready(function($) {
+
+  //Count nr. of square classes
+  var countSquare = $('.square').length;
+
+  //For each Square found add BG image
+  for (i = 0; i < countSquare; i++) {
+    var firstImage = $('.square').eq([i]);
+    var secondImage = $('.square2').eq([i]);
+
+    var getImage = firstImage.attr('data-image');
+    var getImage2 = secondImage.attr('data-image');
+
+    firstImage.css('background-image', 'url(' + getImage + ')');
+    secondImage.css('background-image', 'url(' + getImage2 + ')');
+  }
+
+});
